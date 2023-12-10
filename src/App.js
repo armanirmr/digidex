c257a229ffec03fbd3feb9348e791b17f5547dae
armanirmr <armanirmr@gmail.com>
2023-12-10 17:02:54 -0600

    Added Biome
    Added Biome fast formatter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // App.js
-import React from 'react';
-import DigimonSearch from './DigimonSearch';
-import logo from './digimon-logo.png'
+import React from "react";
+import DigimonSearch from "./DigimonSearch";
+import logo from "./digimon-logo.png";
 
 function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="Logo" />
-      <DigimonSearch />
-    </div>
-  );
+	return (
+		<div className="App">
+			<img src={logo} alt="Logo" />
+			<DigimonSearch />
+		</div>
+	);
 }
 
 export default App;
